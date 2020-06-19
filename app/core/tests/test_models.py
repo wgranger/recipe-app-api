72f85b261f513fb74e9bdfd2f7d2bec66ae2272a
wgranger <twgranger@gmail.com>
2020-06-19 17:36:35 -0500

    Normalize email on uesr creation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,10 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@BIGEMAIL.com'
+        user = get_user_model().objects.create_user(email, '123456')
+
+        self.assertEqual(user.email, email.lower())
