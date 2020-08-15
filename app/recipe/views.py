33e85cd181b3d21371ce5bbb0f00c3d19f0966d8
Will Granger <twgranger@gmail.com>
2020-08-15 12:41:14 -0500

    Create Ingredients Endpoint (#12)
    * Add Ingredient Model to App
    
    * Write Tests for Ingredients Endpoint
    
    * Functioning List Ingredients Feature
    
    * Added Create Ingredient Feature
    
    * Refactor Recipe Views Api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,24 +2,34 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Ingredient, Tag
 
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttrViewset(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for recipe attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for current auth user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new tag"""
+        """Create a new object"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewset):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewset):
+    """Manage ingredients in the db"""
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
